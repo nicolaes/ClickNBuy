9ce0f9fb9360b97cc3d96d22d1273828a01bc570
Nicolae Stihhi <nicolae.stihhi@gmail.com>
2015-09-16 14:41:17 +0300

    Updated the home test

--- app/scripts/components/__tests__/home-test.js
+++ app/scripts/components/__tests__/home-test.js
@@ -11,12 +11,7 @@ describe('Home', function() {
     home = TestUtils.renderIntoDocument(<Home/>);
   });
 
-  it('contains the word React', function() {
-    expect(React.findDOMNode(home).textContent).toContain('React');
-  });
-
-  it('has a list of included items', function() {
-    const items = TestUtils.scryRenderedDOMComponentsWithTag(home, 'li');
-    expect(items.length).toBeGreaterThan(0);
+  it('Has the title ClickNBuy', function() {
+    expect(React.findDOMNode(home).textContent).toContain('ClickNBuy');
   });
 });
